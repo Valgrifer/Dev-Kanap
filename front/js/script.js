ebf3a1f69f834ce151243f156956b3f7f6fee5d4
Valgrifer <valgrifer123@gmail.com>
2022-05-23 14:21:20 +0200

    Insérer les produits dans la page d’accueil

--- front/js/script.js
+++ front/js/script.js
@@ -1,42 +1,16 @@
 (() => {
-    const ORIGIN = "http://valgrifer.fr:3000";
-    const URL = ORIGIN + "/api/products";
-
-    function requestApi(callback, path = "/", method = 'GET')
-    {
-        let params = {
-            method: method,
-            headers: {
-                "Content-Type": "text/plain",
-                "Access-Control-Allow-Origin": "*",
-            },
-            mode: 'no-cors'
-        };
-
-        const request = new Request(URL + path, params);
-
-        fetch(request, params)
-            .then(response => {
-                console.log(response);
-            })
-            // .then(response => {
-            //     if (response.status === 200) {
-            //         return response.json();
-            //     } else {
-            //         throw new Error('Something went wrong on api server!');
-            //     }
-            // })
-            // .then(response => {
-            //     console.debug(response);
-            //     if(callback)
-            //         callback(response);
-            // })
-            .catch(error => {
-                console.error(error);
-            });
-    }
-
     requestApi(response => {
-        console.log(response.length);
+        let content = "";
+        for (const el of response) {
+            content += `<a href="./product.html?id=${el["_id"]}">
+            <article>
+              <img src="${el["imageUrl"]}" alt="${el["altTxt"]}">
+              <h3 class="productName">${el["name"]}</h3>
+              <p class="productDescription">${el["description"]}</p>
+            </article>
+          </a>`;
+        }
+
+        document.querySelector("section#items").innerHTML = content;
     });
 })()
